ddeb0a90cc1d4ab8c353b3a4d73342730e208de3
kassiobarros22@gmail.com <kassiobarros@codilo.com.br>
2019-06-13 18:24:58 -0400

    Recebendo o numero do ultimo capitulo lançado

--- routes.js
+++ routes.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const router = express.Router();
+const rp = require('request-promise');
+const cheerio = require('cheerio')
 
 router.get('/:nome/:capitulo', function (req, res) {
-  res.json({ nome: req.params.nome, capitulo: req.params.capitulo })
+  const nome = req.params.nome;
+  const capitulo = req.params.capitulo
+
+  const pagina = async () => {
+    const result = await rp(`https://www.mangareader.net/${nome}`)
+    const $ = cheerio.load(result);
+    const ultimoCapitulo = $('#latestchapters > ul:nth-child(2) > li:nth-child(1) > a:nth-child(2)').text().trim()
+    const regexNumero = /\w+$/g
+
+    const resultRegex = ultimoCapitulo.match(regexNumero)
+    res.json({
+      nome: req.params.nome,
+      capitulo: req.params.capitulo,
+      totalCapitulos: resultRegex[0]
+    })
+  }
+  pagina()
 })
 
 
