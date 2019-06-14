56be26a51c53715748934cfa684f868bc6ba4e63
kassio <kassiobarros@codilo.com>
2019-06-14 09:06:09 -0400

    Recebendo numero de paginas do capitulo selecionado

--- routes.js
+++ routes.js
@@ -8,16 +8,23 @@ router.get('/:nome/:capitulo', function (req, res) {
   const capitulo = req.params.capitulo
 
   const pagina = async () => {
-    const result = await rp(`https://www.mangareader.net/${nome}`)
-    const $ = cheerio.load(result);
+    const paginaInicial = await rp(`https://www.mangareader.net/${nome}`)
+    const $ = cheerio.load(paginaInicial);
     const ultimoCapitulo = $('#latestchapters > ul:nth-child(2) > li:nth-child(1) > a:nth-child(2)').text().trim()
     const regexNumero = /\w+$/g
 
     const resultRegex = ultimoCapitulo.match(regexNumero)
+
+    const paginaManga = await rp(`https://www.mangareader.net/${nome}/${capitulo}`)
+    const $2 = cheerio.load(paginaManga);
+
+    const numeroPaginas = $2('#pageMenu').children().last().text()
+
     res.json({
       nome: req.params.nome,
       capitulo: req.params.capitulo,
-      totalCapitulos: resultRegex[0]
+      totalCapitulos: resultRegex[0],
+      totalPaginas: numeroPaginas
     })
   }
   pagina()
